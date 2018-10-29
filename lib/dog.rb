e1dabfcd1a765de702225ec53ab0320504318b3d
Manny Pissanos <mpissano@gmail.com>
2018-10-29 16:39:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,4 +5,10 @@ class Dog
     hash.each {|k,v| self.send("#{k}=", v)}
   end
   
+  def self.create_table
+    sql = <<-SQL  
+      CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      n)
+  
 end
